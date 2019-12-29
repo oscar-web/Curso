f55b453a4a1e7c8764a1f1cc50f542016991d2d1
oscarQuiroz <rxlfluf@gmail.com>
2019-12-29 17:56:04 -0500

    Trabajo con seedres y model factory

---
+++ database/factories/PermisoFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Models\Permiso;
+
+
+$factory->define(Permiso::class, function (Faker $faker) {
+    return [
+        'nombre' => $faker->word,
+        'slog' => $faker->word,
+    ];
+});
